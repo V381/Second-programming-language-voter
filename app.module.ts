83836efab5d3ee87b40ff021a859b6b10c265ba2
V381 <paavlee@yahoo.com>
2017-07-29 11:06:14 +0200

    Main Angular modules

---
+++ app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { MainIcons } from './components/app.component';
+import { GetIcons} from "./services/getIcons.service"
+import { SaveScore } from "./services/saveScore.service"
+import { GetIconsScores } from "./services/getIconsScores.service";
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+
+	imports: [BrowserModule, HttpModule, FormsModule],
+	providers : [GetIcons, SaveScore, GetIconsScores, HttpModule]
+	declarations: [MainIcons],
+	bootstrap: [MainIcons]
+})
+
+export class AppModule {
+	
+}
