010e54ccb1bb251f82c7d031aafcba2a84b0af9e
Pavle <V381@users.noreply.github.com>
2017-07-29 13:19:09 +0200

    Update app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { MainIcons } from './components/app.component';
-import { GetIcons} from "./services/getIcons.service"
+import { GetIcons} from "./getIcons.service"
 import { SaveScore } from "./services/saveScore.service"
 import { GetIconsScores } from "./services/getIconsScores.service";
 import { FormsModule } from '@angular/forms';
